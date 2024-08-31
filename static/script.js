fc6ca47b23ed1696a08419252932cedca98ff648
davidwarner404 <testdome404@gmail.com>
2024-09-01 02:14:16 +0530

    new design
    add some changes in layout

--- static/script.js
+++ static/script.js
@@ -1,15 +1,16 @@
 let file1Uploaded = false;
 let file2Uploaded = false;
 
-function uploadFile() {
-    const circuit1 = document.getElementById('circuit1');
-    const circuit3 = document.getElementById('circuit3');
-    const mergePoint = document.getElementById('mergePoint');
+function updateStatus() {
+    const statusLine1 = document.getElementById('statusLine1');
+    const statusLine2 = document.getElementById('statusLine2');
+    const statusCircle = document.getElementById('statusCircle');
     const downloadBtn = document.getElementById('downloadBtn');
+    const pross = document.getElementById('text')
 
     if (document.getElementById('fileInput1').files.length > 0) {
         file1Uploaded = true;
-        circuit1.classList.add('active');
+        statusLine1.style.backgroundColor = '#28a745';
     }
 
     if (document.getElementById('fileInput2').files.length > 0) {
@@ -17,14 +18,14 @@ function uploadFile() {
     }
 
     if (file1Uploaded && file2Uploaded) {
-        mergePoint.classList.add('active');
-        circuit3.classList.add('active');
+        statusCircle.style.backgroundColor = '#FFB318';
+        pross.style.color = '#FFFFFF';
+        pross.innerHTML = 'DONE';
+        statusLine2.style.backgroundColor = '#28a745';
         downloadBtn.disabled = false;
-
-        // Show success alert
         alert('Both files uploaded successfully! Ready to merge and download.');
     }
 }
 
-document.getElementById('fileInput1').addEventListener('change', uploadFile);
-document.getElementById('fileInput2').addEventListener('change', uploadFile);
+document.getElementById('fileInput1').addEventListener('change', updateStatus);
+document.getElementById('fileInput2').addEventListener('change', updateStatus);
